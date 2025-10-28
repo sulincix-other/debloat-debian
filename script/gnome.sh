096bd6af14f48a662a9fd1495ad5ab86043f2082
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2025-10-28 08:24:26 +0000

    debloat

--- script/gnome.sh
+++ script/gnome.sh
@@ -1,6 +1,6 @@
 apt purge --autoremove gnome-shell-extension* -y
 # if gdm3 installed. remove and replace with lightdm
 if dpkg -s gdm3 2>/dev/null ; then
-    apt purge gdm3 --automerove -y
+    apt purge gdm3 --autoremove -y
     apt install lightdm lightdm-gtk-greeter -y
 fi
